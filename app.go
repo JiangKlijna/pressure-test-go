991da70558ba9cb0f6184d51225fb47736bec47d
jiangKlijna <leil729564090@outlook.com>
2018-08-22 00:07:57 +0800

    :fire:add app.go

---
+++ app.go
@@ -0,0 +1,33 @@
+package main
+
+type Application struct {
+}
+
+// Init log & set
+func (app *Application) Init() error {
+	return nil
+}
+
+// check error and exit
+func (app *Application) check(err error) {
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+}
+
+// Start all of server
+func (app *Application) Start() {
+}
+
+// Stop all of server
+func (app *Application) Stop() {
+}
+
+func main() {
+	app := &Application{}
+	err := app.Init()
+	app.check(err)
+	app.Start()
+	app.Stop()
+}

26c893119c9274d8e44bd40c079509af312e70a8
jiangKlijna <leil729564090@outlook.com>
2018-08-22 00:08:06 +0800

    :fire:add setting.go

---
+++ setting.go
@@ -0,0 +1,26 @@
+package main
+
+const defaultJson = `{
+	"task0": {
+		"urls": [
+			{"method": "GET", "url": "http://baidu.com", "params": {}},
+			{"method": "POST", "url": "http://baidu.com", "data": {}},
+		],
+		"headers": {"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"},
+		"init_person": 10,
+		"add_persion": 10,
+		"final_person": 50,
+		"duration": 5
+	},
+    "task1": {
+		"urls": [
+			{"method": "GET", "url": "http://baidu.com", "params": {}},
+			{"method": "POST", "url": "http://baidu.com", "data": {}},
+		],
+		"headers": {"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"},
+		"init_person": 10,
+		"add_persion": 10,
+		"final_person": 50,
+		"duration": 5
+    }
+}

06f964236033fff542ee321c7d6229b30937ea5a
jiangKlijna <leil729564090@outlook.com>
2018-08-27 19:29:18 +0800

    :fire:add task.go

---
+++ task.go
@@ -0,0 +1,20 @@
+package main
+
+type TaskServer struct {
+	setting *TaskSetting
+}
+
+// go t.start()
+func (t *TaskServer) run() {
+
+}
+
+// start server
+func (t *TaskServer) start() {
+
+}
+
+// stop server
+func (t *TaskServer) stop() {
+
+}

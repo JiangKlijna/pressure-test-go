77a695119a3570b3c10e9152784f6c99ae19c1ca
jiangKlijna <leil729564090@outlook.com>
2018-08-27 19:08:12 +0800

    :fire:fix

--- generate.go
+++ generate.go
@@ -7,10 +7,12 @@ type PressureTestResult struct {
 	duration_time  float32
 }
 
-func (r PressureTestResult) success_rate() int {
-	return (r.request_number - r.failure_number) / r.request_number * 100
+// get Success rate
+func (r PressureTestResult) success_rate() float32 {
+	return float32(r.request_number - r.failure_number) / float32(r.request_number) * 100
 }
 
+// get Average time consuming
 func (r PressureTestResult) average_time() float32 {
 	return float32(r.request_number) / r.duration_time
 }

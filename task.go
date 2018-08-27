a713713974a7d3b2d2ea5cd95f0d693b650df175
jiangKlijna <leil729564090@outlook.com>
2018-08-27 19:46:24 +0800

    :fire:update task

--- task.go
+++ task.go
@@ -1,20 +1,29 @@
 package main
 
 type TaskServer struct {
+	isRun   bool
 	setting *TaskSetting
 }
 
-// go t.start()
+func NewTaskServer(setting *TaskSetting) *TaskServer {
+	return &TaskServer{true, setting}
+}
+
+// single request
 func (t *TaskServer) run() {
 
 }
 
 // start server
 func (t *TaskServer) start() {
-
+	go func() {
+		for t.isRun {
+			t.run()
+		}
+	}()
 }
 
 // stop server
 func (t *TaskServer) stop() {
-
+	t.isRun = false
 }

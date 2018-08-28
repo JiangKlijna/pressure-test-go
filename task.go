333e85db57ac1e13c860188f8f2df3dc90256411
jiangKlijna <leil729564090@outlook.com>
2018-08-28 18:45:13 +0800

    :fire:update TaskPerson

--- task.go
+++ task.go
@@ -5,13 +5,24 @@ type TaskServer struct {
 	setting *TaskSetting
 }
 
+func (t *TaskServer) start() {
+
+}
+
+func (t *TaskServer) stop() {
+	for _, p := range t.person {
+		p.stop()
+	}
+}
+
 func NewTaskServer(setting *TaskSetting) *TaskServer {
 	return &TaskServer{make([]TaskPerson, setting.Final_person), setting}
 }
 
 type TaskPerson struct {
-	isRun bool
-	urls  []map[string]interface{}
+	isRun  bool
+	isStop bool
+	task   *TaskServer
 }
 
 // single request
@@ -22,9 +33,11 @@ func (t *TaskPerson) run() {
 // start server
 func (t *TaskPerson) start() {
 	go func() {
+		t.isRun = true
 		for t.isRun {
 			t.run()
 		}
+		t.isStop = true
 	}()
 }
 

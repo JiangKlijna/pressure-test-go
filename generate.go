0fc22dea19c1a163f96af463a2dd47beb1c6ba07
jiangKlijna <leil729564090@outlook.com>
2018-10-14 15:27:32 +0800

    :file:add GolangFormater

--- generate.go
+++ generate.go
@@ -68,10 +68,16 @@ func JsonFormater(res []*PressureTestResult, f os.File) {
 	f.Write(data)
 }
 
+func GolangFormater(res []*PressureTestResult, f os.File) {
+	defer f.Close()
+	data := []byte(fmt.Sprintf("%s", res))
+	f.Write(data)
+}
+
 func csvMarshal(res []*PressureTestResult) ([]byte, error) {
 	buf := bytes.Buffer{}
 	buf.WriteString("Id,RequestNumber,FailureNumber,DurationTime\n")
-	for _, r := range res  {
+	for _, r := range res {
 		buf.WriteString(fmt.Sprintf("%d,%d,%d,%s\n", r.Id, r.RequestNumber, r.FailureNumber, r.DurationTime))
 	}
 	return buf.Bytes(), nil
@@ -80,7 +86,7 @@ func csvMarshal(res []*PressureTestResult) ([]byte, error) {
 func htmlMarshal(res []*PressureTestResult) ([]byte, error) {
 	buf := bytes.Buffer{}
 	buf.WriteString("<html><head><title>压力测试结果报告</title><meta chararset=\"utf-8\" /><style>*{margin:0;padding:0;font-family:consolas}tr:nth-of-type(odd){background:#e8edff}tr:nth-of-type(even){background:white}tr:nth-of-type(odd):hover{background:white}tr:nth-of-type(even):hover{background:#e8edff}table{width:100%;text-align:center;color:#669}tr:first-of-type,tr:last-of-type{font-size:18px;font-weight:900}tr:hover{cursor:pointer}td{padding:8px}</style></head><body><table><tr><td>任务</td><td>请求总数</td><td>失败总数</td><td>成功率</td><td>平均耗时(s)</td></tr>")
-	for _, r := range res  {
+	for _, r := range res {
 		buf.WriteString(fmt.Sprintf("<tr><td>%d</td><td>%d</td><td>%d</td><td>%s</td></tr>", r.Id, r.RequestNumber, r.FailureNumber, r.DurationTime))
 	}
 	buf.WriteString("</table></body></html>")
